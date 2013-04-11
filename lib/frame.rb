741086f8eb44efbbc0c14c46f7c0681b08058ee2
Edward Funger <teddythetwig@gmail.com>
2013-04-11 14:02:07 -0400

    removed stuff

--- lib/frame.rb
+++ lib/frame.rb
@@ -6,8 +6,9 @@
 module BtceTrader
   class Frame < ActiveRecord::Base
     has_many :trades
+    has_one :tickers
     #to be implemented      
-    #has_one :ticker
+    
     #has_one :depth
     #has_one :someothershit
     attr_accessible :start, :end, :pair, :frame_id
