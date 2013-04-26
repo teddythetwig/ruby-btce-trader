523a7ee60713ed579aeedbdcda8265a6baa0ac67
Edward Funger <teddythetwig@gmail.com>
2013-04-25 21:42:22 -0400

    commit incase crash

--- lib/runner.rb
+++ lib/runner.rb
@@ -5,23 +5,32 @@ module BtceTrader
     def self.update_data
       time = Time.now.localtime("+04:00")
       puts "Updating data at #{time}"
+  
       Algorithm::PAIRS.each do |pair|
         new_frame = BtceTrader::Frame.create(:start => time.to_i - INTERVAL, :end => time.to_i, :pair => pair)
         Algorithm::OPERATIONS.each do |operation|
           response = Btce::PublicAPI.get_pair_operation_json(pair,operation)
-          #this is getting messy
-          if operation == "trades"
-            slice_index = response.rindex{|trade| ((time.to_i - INTERVAL)..time.to_i) === trade["date"] }
-            slice_index = response.size if slice_index.nil?
-            trades = response.first(slice_index + 1) 
-            ActiveRecord::Base.transaction do
+          #this is gettinge messy
+          
+          ActiveRecord::Base.transaction do
+            if operation == "trades"
+              slice_index = response.rindex{|trade| !(((time.to_i - INTERVAL)..time.to_i) === trade["date"].to_i) }
+              puts slice_index
+              slice_index = response.size if slice_index.nil?
+              trades = response.first(slice_index + 1)
+              puts "Size of new trades is #{trades.size}"
+              puts "Range: #{time.to_i - INTERVAL} - #{time.to_i}"
+              puts "First:#{trades.last['date']}"
+              puts "Last:#{trades.first['date']}"
               trades.each do |trade|
                 new_frame.trades.create(trade)
+                
               end
+              
+            elsif operation == "ticker"
+              ticker = response[operation]
+              #new_frame.tickers.create(ticker)
             end
-          elsif operation == "ticker"
-            ticker = response[operation]
-            #new_frame.tickers.create(ticker)
           end
         end
       end
